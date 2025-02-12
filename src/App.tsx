fab726d7d08eab109b236f074e953b946ca06ca9
Gabriel Goldas <gabriel.goldas@gmail.com>
2025-02-12 11:04:10 -0300

    create TaskBoard and Task Card

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Box, Flex, Heading } from "@radix-ui/themes"
 import { CreateTaskForm } from "./components/CreateTaskForm"
+import { TaskBoard } from "./components/TaksBoard"
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
       </Box>
 
       <Box>
-        <Heading as="h2">Quadro de tarefas</Heading>
+        <Heading as="h2" mb="4">Quadro de tarefas</Heading>
+        <TaskBoard />
       </Box>
     </Box>
   )
--- src/components/CreateTaskForm.tsx
+++ src/components/CreateTaskForm.tsx
@@ -1,7 +1,34 @@
 import { PlusIcon } from "@radix-ui/react-icons"
 import { Badge, Box, Button, Dialog, Flex, RadioGroup, Text, TextArea, TextField } from "@radix-ui/themes"
+import { FormEventHandler } from "react"
+import { z } from "zod"
+
+const CreateTaskSchema = z.object({
+  title: z.string(),
+  description: z.string(),
+  status: z.enum(["todo", "doing", "done"]),
+  priority: z.enum(["low", "medium", "high"])
+})
 
 export const CreateTaskForm: React.FC = () => {
+
+  const handleSubmit: FormEventHandler<HTMLFormElement> = async (ev) => {
+    ev.preventDefault()
+
+    const formData = new FormData(ev.currentTarget)
+
+    const title = formData.get("title") // name do input
+    const description = formData.get("description") // name do input
+    const status = formData.get("status") // name do input
+    const priority = formData.get("priority") // name do input
+
+    ev.currentTarget.reset()
+
+    const taskData = CreateTaskSchema.parse({ title, description, status, priority })
+    alert(JSON.stringify(taskData))
+
+  }
+
   return (
     <Dialog.Root>
       <Dialog.Trigger>
@@ -16,7 +43,7 @@ export const CreateTaskForm: React.FC = () => {
           Adicione novas tarefas ao quadro.
         </Dialog.Description>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <Flex direction="column" gap="4">
             <Box maxWidth="32rem">
               <Box mb="2">
